1b3c1e4aeb06edfde1bf222f1fc63ad2b4b218b2
Nizar Chaouachi <nizar.chawechy.1618@gmail.com>
2023-11-30 10:55:01 +0100

    added

--- dci_social/social_project/social_app/views_1.py
+++ dci_social/social_project/social_app/views_1.py
@@ -1,11 +1,14 @@
 from typing import Any
+from django.db import models
 from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView,UpdateView,CreateView
 from django.http import HttpResponse
 from .models import User, Post
-
+from django.urls import reverse_lazy
+from .form_1 import UserForm,PostForm
+ 
 # class HomePageView(View):
 #     def get(self,request):
 #         return HttpResponse('<h1>finally weekend !</h1>')
@@ -65,6 +68,37 @@ class UserPostsView(ListView):
         username = self.kwargs['username']
         user = get_object_or_404(User , username = username)
         return Post.objects.filter(user=user).order_by('-created_at')
+    
+    
+class CreateUserView(CreateView):
+    model = User
+    form_class = UserForm
+    template_name ='social_app/user_form.html'
+    success_url = reverse_lazy('user_list')
+    
+class CreatePostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'social_app/post_form.html'
+    def form_valid(self,form):
+        user = get_object_or_404(User,username=self.kwargs['username'])
+        form.instance.user = user
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('user_detail',kwargs ={'username':self.object.user.username})
+    
+    
+class UpdateUserView(UpdateView):
+    model = User
+    form_class = UserForm
+    template_name = 'social_app/user_form.html'
+    
+    def get_object(self, queryset=None):
+        return get_object_or_404(User,username = self.kwargs['username'])
+    def get_success_url(self):
+        return reverse_lazy('user_detail', kwargs={'username': self.object.username})
+    
         
     
     
--- dci_social/social_project/social_project/urls.py
+++ dci_social/social_project/social_project/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('social_app.urls')),
-    #path('',include('social_app.urls_1'))
+    #path('',include('social_app.urls')),
+    path('app/',include('social_app.urls_1'))
     
 ] 
